6b86371d6bad97d919b8141a8f56d5282bb00fac
SquadQuiz <amornsakstp6@gmail.com>
2024-07-09 00:46:59 +0700

    Change: Entry style to ttk theme

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# Import tkinter GUI
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
+
+# Import datetime
 from datetime import datetime
 # Import Database
 from db_maintenance import *
@@ -20,42 +24,42 @@ L.pack()
 L = Label(GUI, text="Name:", font=FONT2)
 L.place(x=30, y=50)
 v_name = StringVar()  # special variable for GUI
-E1 = Entry(GUI, textvariable=v_name, font=FONT2)
+E1 = ttk.Entry(GUI, textvariable=v_name, font=FONT2)
 E1.place(x=150, y=50)
 
 # ---------------
 L = Label(GUI, text="Department:", font=FONT2)
 L.place(x=30, y=100)
 v_department = StringVar()
-E2 = Entry(GUI, textvariable=v_department, font=FONT2)
+E2 = ttk.Entry(GUI, textvariable=v_department, font=FONT2)
 E2.place(x=150, y=100)
 
 # ---------------
 L = Label(GUI, text="Equipment/Tools:", font=FONT2)
 L.place(x=30, y=150)
 v_tools = StringVar()
-E3 = Entry(GUI, textvariable=v_tools, font=FONT2)
+E3 = ttk.Entry(GUI, textvariable=v_tools, font=FONT2)
 E3.place(x=150, y=150)
 
 # ---------------
 L = Label(GUI, text="Details:", font=FONT2)
 L.place(x=30, y=200)
 v_details = StringVar()
-E4 = Entry(GUI, textvariable=v_details, font=FONT2)
+E4 = ttk.Entry(GUI, textvariable=v_details, font=FONT2)
 E4.place(x=150, y=200)
 
 # ---------------
 L = Label(GUI, text="Part number:", font=FONT2)
 L.place(x=30, y=250)
 v_part_number = StringVar()
-E5 = Entry(GUI, textvariable=v_part_number, font=FONT2)
+E5 = ttk.Entry(GUI, textvariable=v_part_number, font=FONT2)
 E5.place(x=150, y=250)
 
 # ---------------
 L = Label(GUI, text="Phone number:", font=FONT2)
 L.place(x=30, y=300)
 v_phone_number = StringVar()
-E6 = Entry(GUI, textvariable=v_phone_number, font=FONT2)
+E6 = ttk.Entry(GUI, textvariable=v_phone_number, font=FONT2)
 E6.place(x=150, y=300)
 
 # ---------------
@@ -90,7 +94,11 @@ def clear_stringvar_value():
     v_phone_number.set('')
 
 #### Button ####
-B = Button(GUI, text="   Save   ", font=FONT2, command=save_button)
+# Create a style and configure the font for the TButton
+style = ttk.Style()
+style.configure("TButton", font=FONT2)
+
+B = ttk.Button(GUI, text="   Save   ", style="TButton", command=save_button)
 B.place(x=200, y=350)
 
 #### Main loop ####
