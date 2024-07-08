6aec8fdae0593f6210bc8ecdb6f2718f65f35ad9
SquadQuiz <amornsakstp6@gmail.com>
2024-07-09 00:52:30 +0700

    Changes: Current widgets will under T1 or Tab 1

--- main.py
+++ main.py
@@ -29,52 +29,52 @@ Tab.pack(fill=BOTH, expand=1)
 ####------------------ TAB1 ------------------###
 
 #### Label & Entry ####
-L = Label(GUI, text="Maintenance Tickets!", font=FONT1)
+L = Label(T1, text="Maintenance Tickets!", font=FONT1)
 L.pack()
 
 # ---------------
-L = Label(GUI, text="Name:", font=FONT2)
+L = Label(T1, text="Name:", font=FONT2)
 L.place(x=30, y=50)
-v_name = StringVar()  # special variable for GUI
-E1 = ttk.Entry(GUI, textvariable=v_name, font=FONT2)
+v_name = StringVar()  # special variable for T1
+E1 = ttk.Entry(T1, textvariable=v_name, font=FONT2)
 E1.place(x=150, y=50)
 
 # ---------------
-L = Label(GUI, text="Department:", font=FONT2)
+L = Label(T1, text="Department:", font=FONT2)
 L.place(x=30, y=100)
 v_department = StringVar()
-E2 = ttk.Entry(GUI, textvariable=v_department, font=FONT2)
+E2 = ttk.Entry(T1, textvariable=v_department, font=FONT2)
 E2.place(x=150, y=100)
 
 # ---------------
-L = Label(GUI, text="Equipment/Tools:", font=FONT2)
+L = Label(T1, text="Equipment/Tools:", font=FONT2)
 L.place(x=30, y=150)
 v_tools = StringVar()
-E3 = ttk.Entry(GUI, textvariable=v_tools, font=FONT2)
+E3 = ttk.Entry(T1, textvariable=v_tools, font=FONT2)
 E3.place(x=150, y=150)
 
 # ---------------
-L = Label(GUI, text="Details:", font=FONT2)
+L = Label(T1, text="Details:", font=FONT2)
 L.place(x=30, y=200)
 v_details = StringVar()
-E4 = ttk.Entry(GUI, textvariable=v_details, font=FONT2)
+E4 = ttk.Entry(T1, textvariable=v_details, font=FONT2)
 E4.place(x=150, y=200)
 
 # ---------------
-L = Label(GUI, text="Part number:", font=FONT2)
+L = Label(T1, text="Part number:", font=FONT2)
 L.place(x=30, y=250)
 v_part_number = StringVar()
-E5 = ttk.Entry(GUI, textvariable=v_part_number, font=FONT2)
+E5 = ttk.Entry(T1, textvariable=v_part_number, font=FONT2)
 E5.place(x=150, y=250)
 
 # ---------------
-L = Label(GUI, text="Phone number:", font=FONT2)
+L = Label(T1, text="Phone number:", font=FONT2)
 L.place(x=30, y=300)
 v_phone_number = StringVar()
-E6 = ttk.Entry(GUI, textvariable=v_phone_number, font=FONT2)
+E6 = ttk.Entry(T1, textvariable=v_phone_number, font=FONT2)
 E6.place(x=150, y=300)
 
-# --------- Function --------- #
+# ---------------
 def clear_stringvar_value():
     v_name.set('')
     v_department.set('')
@@ -110,8 +110,10 @@ def save_button():
 style = ttk.Style()
 style.configure("TButton", font=FONT2)
 
-B = ttk.Button(GUI, text="   Save   ", style="TButton", command=save_button)
+B = ttk.Button(T1, text="   Save   ", style="TButton", command=save_button)
 B.place(x=200, y=350)
+
+####------------------ END-TAB1 ------------------###
 
 #----------------------- Main loop -----------------------#
 GUI.mainloop()
