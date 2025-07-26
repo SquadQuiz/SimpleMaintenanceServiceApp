4ecf8652c4cef8e0b6f2a83ffe3c27975140658f
SquadQuiz <amornsakstp6@gmail.com>
2025-07-27 02:18:24 +0700

    feat: enhance entry navigation and save functionality in maintenance ticket form

--- main.py
+++ main.py
@@ -60,12 +60,16 @@ v_name = StringVar()  # special variable for T1
 E1 = ttk.Entry(T1, textvariable=v_name, font=FONT2)
 E1.place(x=150, y=50)
 
+E1.bind('<Return>', lambda event: E2.focus())
+
 # ---------------
 L = Label(T1, text="Department:", font=FONT2)
 L.place(x=30, y=100)
 v_department = StringVar()
 E2 = ttk.Entry(T1, textvariable=v_department, font=FONT2)
 E2.place(x=150, y=100)
+
+E2.bind('<Return>', lambda event: E3.focus())
 
 # ---------------
 L = Label(T1, text="Equipment/Tools:", font=FONT2)
@@ -74,6 +78,8 @@ v_tools = StringVar()
 E3 = ttk.Entry(T1, textvariable=v_tools, font=FONT2)
 E3.place(x=150, y=150)
 
+E3.bind('<Return>', lambda event: E4.focus())
+
 # ---------------
 L = Label(T1, text="Details:", font=FONT2)
 L.place(x=30, y=200)
@@ -81,12 +87,16 @@ v_details = StringVar()
 E4 = ttk.Entry(T1, textvariable=v_details, font=FONT2)
 E4.place(x=150, y=200)
 
+E4.bind('<Return>', lambda event: E5.focus())
+
 # ---------------
 L = Label(T1, text="Part number:", font=FONT2)
 L.place(x=30, y=250)
 v_part_number = StringVar()
 E5 = ttk.Entry(T1, textvariable=v_part_number, font=FONT2)
 E5.place(x=150, y=250)
+
+E5.bind('<Return>', lambda event: E6.focus())
 
 # ---------------
 L = Label(T1, text="Phone number:", font=FONT2)
@@ -104,7 +114,7 @@ def clear_stringvar_value():
     v_part_number.set('')
     v_phone_number.set('')
 
-def save_button():
+def save_button(event=None):
     name = v_name.get()
     department = v_department.get()
     tools = v_tools.get()
@@ -128,6 +138,9 @@ def save_button():
     # Clear string
     clear_stringvar_value()
     update_table()
+    
+    # Focus on the first entry field
+    E1.focus()
 
 #### Button ####
 # Create a style and configure the font for the TButton
@@ -136,6 +149,9 @@ style.configure("TButton", font=FONT2)
 
 B = ttk.Button(T1, text="   Save   ", style="TButton", command=save_button)
 B.place(x=200, y=350)
+
+# Bind the Return key to the save_button function
+E6.bind('<Return>', save_button)
 
 ####------------------ TAB2-Tickets Information ------------------###
 #TODO: Tree-view table 
@@ -393,6 +409,8 @@ def new_note(event):
     get_data_db = view_mtnote_tsid(tsid)
     print(get_data_db)
     
+    GUI3.bind('<F1>', lambda event: GUI3.focus())  # Bind F1 key to focus on the dialog
+    
     # Insert data to textbox
     if (get_data_db != None):
         cal.set_date(get_data_db[2]) # get date from database
@@ -402,10 +420,16 @@ def new_note(event):
         messagebox.showwarning('Warning', 'No data found!')
     
     def save_note():
+        # check state if data is new or update
         date = cal.get()
         details = E2.get('1.0', 'end').strip() # strip '\n' from textbox
         other = E3.get('1.0', 'end').strip()
-        insert_mtnote(tsid, date, details, other)
+        if (get_data_db == None):
+            insert_mtnote(tsid, date, details, other)
+        else:
+            update_mtnote(tsid, 'date_start', date) 
+            update_mtnote(tsid, 'details', details)
+            update_mtnote(tsid, 'other', other)
         messagebox.showinfo('Save', 'Saving information...')
         GUI3.destroy()
         update_table_approved_repairs(approved_repairs_table)
